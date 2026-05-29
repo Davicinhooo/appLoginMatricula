a493f24bb54571318ea12505679b52a011ef53a7
Walter Guevara <wd123456689@icloud.com>
2026-05-29 00:45:22 -0500

    Resolvi algunos problemas con el boton de cerrar sesion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,13 +48,12 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-end border-0 shadow-sm" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item text-danger" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
+                                <a class="dropdown-item" href="{{ route('logout') }}" 
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                     Cerrar Sesión
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
                             </div>
